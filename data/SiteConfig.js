23be6c196b70072815569427403477e9e227a26f
Austin Lanari <austin.lanari@gmail.com>
2018-01-07 23:12:42 -0500

    update prefix

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,8 +4,8 @@ module.exports = {
   siteTitleAlt: "Austin Lanari's Professional Site", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://austinlanari.com", // Domain of your website without pathPrefix.
-  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "A GatsbyJS stater with Advanced design in mind.", // Website description used for RSS feeds/meta description tag.
+  pathPrefix: "/personal-gatsby", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: "Professional website of Austin Lanari.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   // siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-110790727-2", // GA tracking ID.
