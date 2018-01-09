0d0dc12497d4199dffb7a791b80e86b4a9b6702c
Austin Lanari <austin.lanari@gmail.com>
2018-01-08 23:21:39 -0500

    basic fontawesome icons

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -22,17 +22,17 @@ module.exports = {
     {
       label: "GitHub",
       url: "https://github.com/foggy1",
-      iconClassName: "fa fa-github"
+      iconClassName: "github"
     },
     {
       label: "Twitter",
       url: "https://twitter.com/AustinLanari",
-      iconClassName: "fa fa-twitter"
+      iconClassName: "twitter"
     },
     {
       label: "Email",
       url: "mailto:austin.lanari@gmail.com",
-      iconClassName: "fa fa-envelope"
+      iconClassName: "envelope"
     }
   ],
   copyright: "Copyright © 2017. Austin Lanari", // Copyright string for the footer of the website and RSS feed.
