1957a78e27f12c819d941bbc9196b1f1888b6066
Austin Lanari <austin.lanari@gmail.com>
2018-01-11 23:24:38 -0500

    footer and licenses

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -71,6 +71,16 @@ export default class MainLayout extends React.Component {
           </div>
         </section>
         {children()}
+        <footer className="footer">
+          <div className="container">
+            <div className="content has-text-centered">
+              <p>
+                Website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"> CC BY NC SA 4.0</a> Austin Lanari.
+                Website code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+              </p>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
