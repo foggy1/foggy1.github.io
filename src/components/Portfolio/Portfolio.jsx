34a37f7bf014a8171676df3c34ab042042e40ce5
Austin Lanari <austin.lanari@gmail.com>
2018-01-17 00:17:30 -0500

    text

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -13,16 +13,12 @@ import sass from './sass.svg'
 
 class About extends Component {
   render() {
-    console.log(graphql)
     return (
       <section className="hero is-dark">
         <div className='hero-body'>
-          <div class="container has-text-centered">
-            <h1 class="title">
-              Title
-            </h1>
-            <h2 class="subtitle">
-              Subtitle
+          <div className="container has-text-centered">
+            <h2 className="subtitle">
+              I'm a Full-Stack Web Developer
             </h2>
             <div className='columns is-mobile is-centered'>
               <div className='column is-three-quarters-mobile is-narrow is-mobile'>
@@ -30,7 +26,7 @@ class About extends Component {
                   <li className='column is-one-third-mobile'>
                     <img style={{margin: 'auto'}}
                       className='image is-48x48'
-                      src={graphql}
+                      src={javascript}
                     />
                   </li>
                   <li className='column is-one-third-mobile'>
@@ -42,43 +38,7 @@ class About extends Component {
                   <li className='column is-one-third-mobile'>
                     <img style={{margin: 'auto'}}
                       className='image is-48x48'
-                      src={javascript}
-                    />
-                  </li>
-                  <li className='column is-one-third-mobile'>
-                    <img style={{margin: 'auto'}}
-                      className='image is-48x48'
-                      src={redux}
-                    />
-                  </li>
-                  <li className='column is-one-third-mobile'>
-                    <img style={{margin: 'auto'}}
-                      className='image is-48x48'
                       src={react}
-                    />
-                  </li>
-                  <li className='column is-one-third-mobile'>
-                    <img style={{margin: 'auto'}}
-                      className='image is-48x48'
-                      src={css}
-                    />
-                  </li>
-                  <li className='column is-one-third-mobile'>
-                    <img style={{margin: 'auto'}}
-                      className='image is-48x48'
-                      src={html}
-                    />
-                  </li>
-                  <li className='column is-one-third-mobile'>
-                    <img style={{margin: 'auto'}}
-                      className='image is-48x48'
-                      src={gatsby}
-                    />
-                  </li>
-                  <li className='column is-one-third-mobile'>
-                    <img style={{margin: 'auto'}}
-                      className='image is-48x48'
-                      src={lambda}
                     />
                   </li>
                   <li className='column is-one-third-mobile'>
@@ -90,10 +50,49 @@ class About extends Component {
                   <li className='column is-one-third-mobile'>
                     <img style={{margin: 'auto'}}
                       className='image is-48x48'
+                      src={html}
+                    />
+                  </li>
+                  <li className='column is-one-third-mobile'>
+                    <img style={{margin: 'auto'}}
+                      className='image is-48x48'
+                      src={redux}
+                    />
+                  </li>
+                  <li className='column is-one-third-mobile'>
+                    <img style={{margin: 'auto'}}
+                      className='image is-48x48'
+                      src={css}
+                    />
+                  </li>
+                  <li className='column is-one-third-mobile'>
+                    <img style={{margin: 'auto'}}
+                      className='image is-48x48'
+                      src={lambda}
+                    />
+                  </li>
+                  <li className='column is-one-third-mobile'>
+                    <img style={{margin: 'auto'}}
+                      className='image is-48x48'
+                      src={gatsby}
+                    />
+                  </li>
+                  <li className='column is-one-third-mobile'>
+                    <img style={{margin: 'auto'}}
+                      className='image is-48x48'
+                      src={graphql}
+                    />
+                  </li>
+                  <li className='column is-one-third-mobile'>
+                    <img style={{margin: 'auto'}}
+                      className='image is-48x48'
                       src={sass}
                     />
                   </li>
                 </ul>
+                <h2 className="subtitle">
+                  I know stuff.
+                </h2>
               </div>
             </div>
           </div>
